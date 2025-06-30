ef44195865c089de8e7e674e9fe1c526cdd8d8e1
pawan-vishwakarma <pawanvishwakarma31577@gmail.com>
2025-07-01 02:14:29 +0530

    added test

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,16 +4,16 @@ from sklearn.preprocessing import LabelEncoder
 def load_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
-def preprocess_data(df: pd.DataFrame) -> tuple:
-    df = df.copy()
-    if 'customerID' in df.columns:
-        df.drop('customerID', axis=1, inplace=True)
+# def preprocess_data(df: pd.DataFrame) -> tuple:
+#     df = df.copy()
+#     if 'customerID' in df.columns:
+#         df.drop('customerID', axis=1, inplace=True)
 
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-    df.fillna(df.median(numeric_only= True), inplace=True)
+#     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+#     df.fillna(df.median(numeric_only= True), inplace=True)
 
-    cat_cols = df.select_dtypes(include='object').columns.tolist()
-    encoders={}
+#     cat_cols = df.select_dtypes(include='object').columns.tolist()
+#     encoders={}
 
     for col in cat_cols:
         le = LabelEncoder()
